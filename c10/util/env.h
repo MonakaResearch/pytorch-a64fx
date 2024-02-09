315e42ae2babf66ad7145ea25a7bf9a5bf0a31c2
cyy <cyyever@outlook.com>
2024-02-09 23:05:22 +0800

    Improve doc

--- c10/util/env.h
+++ c10/util/env.h
@@ -15,9 +15,6 @@ C10_API void set_env(
 // Checks an environment variable is set.
 C10_API bool has_env(const char* name);
 
-// Reads an environment variable and returns the content if it is set.
-C10_API std::optional<std::string> get_env(const char* name);
-
 // Reads an environment variable and returns
 // - optional<true>,              if set equal to "1"
 // - optional<false>,             if set equal to "0"
@@ -26,4 +23,9 @@ C10_API std::optional<std::string> get_env(const char* name);
 // NB:
 // Issues a warning if the value of the environment variable is not 0 or 1.
 C10_API std::optional<bool> check_env(const char* name);
+
+// Reads the value of an environment variable if it is set.
+// However, check_env should be used if the value is assumed to be a flag.
+C10_API std::optional<std::string> get_env(const char* name);
+
 } // namespace c10::utils
