c6a37426b8725c4c6b0681f4f2510197516ee12f
Peter Y Yeh <peter.yeh@gmail.com>
2024-03-28 22:26:55 +0000

    lint

--- c10/cuda/driver_api.cpp
+++ c10/cuda/driver_api.cpp
@@ -11,28 +11,28 @@ namespace c10::cuda {
 namespace {
 
 DriverAPI create_driver_api() {
-    c10::string_view libName = (USE_ROCM) ? "libamdhip64.so" : "libcuda.so.1";
-    void* handle_0 = dlopen(libName.data(), RTLD_LAZY | RTLD_NOLOAD);
-    TORCH_CHECK(handle_0, "Can't open libs", dlerror());
-    void* handle_1 = DriverAPI::get_nvml_handle();
-    DriverAPI r{};
+  c10::string_view libName = (USE_ROCM) ? "libamdhip64.so" : "libcuda.so.1";
+  void* handle_0 = dlopen(libName.data(), RTLD_LAZY | RTLD_NOLOAD);
+  TORCH_CHECK(handle_0, "Can't open libs", dlerror());
+  void* handle_1 = DriverAPI::get_nvml_handle();
+  DriverAPI r{};
 
-    #define LOOKUP_LIBCUDA_ENTRY(name)                       \
-      r.name##_ = ((decltype(&name))dlsym(handle_0, #name)); \
-      TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
-      C10_LIBCUDA_DRIVER_API(LOOKUP_LIBCUDA_ENTRY)
-    #undef LOOKUP_LIBCUDA_ENTRY
+#define LOOKUP_LIBCUDA_ENTRY(name)                       \
+  r.name##_ = ((decltype(&name))dlsym(handle_0, #name)); \
+  TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
+  C10_LIBCUDA_DRIVER_API(LOOKUP_LIBCUDA_ENTRY)
+#undef LOOKUP_LIBCUDA_ENTRY
 
-    #ifndef USE_ROCM
-      if (handle_1) {
-    #define LOOKUP_NVML_ENTRY(name)                          \
-      r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); \
-      TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
-        C10_NVML_DRIVER_API(LOOKUP_NVML_ENTRY)
-    #undef LOOKUP_NVML_ENTRY
-      }
-    #endif
-      return r;
+#ifndef USE_ROCM
+  if (handle_1) {
+#define LOOKUP_NVML_ENTRY(name)                          \
+  r.name##_ = ((decltype(&name))dlsym(handle_1, #name)); \
+  TORCH_INTERNAL_ASSERT(r.name##_, "Can't find ", #name, ": ", dlerror())
+    C10_NVML_DRIVER_API(LOOKUP_NVML_ENTRY)
+#undef LOOKUP_NVML_ENTRY
+  }
+#endif
+  return r;
 }
 } // namespace
 
