d3167cf5f52635cd88f5be1cd08b750908f823d8
Jiashen Cao <jiashenc@meta.com>
2024-06-27 13:44:53 -0700

    fix lint

--- test/export/test_converter.py
+++ test/export/test_converter.py
@@ -414,9 +414,9 @@ class TestConverter(TestCase):
 
         inp = (torch.ones(3),)
         orig_m = NestedM(3)
-        ep = self._check_equal_ts_ep_converter(orig_m, inp)
+        self._check_equal_ts_ep_converter(orig_m, inp)
         orig_m = SuperNestedM(3)
-        ep = self._check_equal_ts_ep_converter(orig_m, inp)
+        self._check_equal_ts_ep_converter(orig_m, inp)
 
     def test_convert_nn_module_with_nested_buffer(self):
         class M(torch.nn.Module):
@@ -799,6 +799,7 @@ class TestConverter(TestCase):
         empty = torch.tensor([], dtype=torch.double)
         values = [empty] + [torch.randn(M, N) for N in Ns]
         self._check_equal_ts_ep_converter(func2, tuple(values), ["trace"])
+
 
 if __name__ == "__main__":
     run_tests()
